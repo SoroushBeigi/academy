fce061d9eaa111c35ba88566b7a9cbd3d92da980
s.masoumbeigi <sbeygi712@gmail.com>
2024-06-09 15:03:56 +0330

    saved page web version UI

--- lib/src/features/favourite/presentation/pages/web/favourite_web_page.dart
+++ lib/src/features/favourite/presentation/pages/web/favourite_web_page.dart
@@ -1,6 +1,10 @@
+import 'package:academy/src/core/resources/value_manager.dart';
+import 'package:academy/src/features/favourite/presentation/cubit/favourite_cubit.dart';
+import 'package:academy/src/features/favourite/presentation/cubit/favourite_state.dart';
 import 'package:academy/src/features/video_details/presentation/pages/widgets/related_video/related_video_container.dart';
-import 'package:academy/video_model.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class FavouriteWebPage extends StatelessWidget {
   const FavouriteWebPage({super.key});
@@ -8,30 +12,40 @@ class FavouriteWebPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final crossCount = (MediaQuery.of(context).size.width ~/ 350).toInt();
-    return Scaffold(
-        body: GridView.count(
-      crossAxisCount: crossCount == 0 ? 1 : crossCount,
-      childAspectRatio: 1.18,
-      children: [
-        RelatedVideoContainer(
-          videoModel: VideoModel(
-              title: 'Golang Tutorial',
-              imageUrl:
-                  "https://mestergraph.com/uploads/pictures/teklonozhiiiiiiiiiiii/shabake/master_groph_8-8_1.jpg",
-              viewCount: 24,
-              duration: '45:20',
-              publishTime: DateTime.now().subtract(const Duration(days: 45))),
-        ),
-        RelatedVideoContainer(
-          videoModel: VideoModel(
-              title: 'Golang Tutorial',
-              imageUrl:
-                  "https://mestergraph.com/uploads/pictures/teklonozhiiiiiiiiiiii/shabake/master_groph_8-8_1.jpg",
-              viewCount: 24,
-              duration: '45:20',
-              publishTime: DateTime.now().subtract(const Duration(days: 45))),
-        ),
-      ],
-    ));
+    return BlocProvider(
+        create: (context) => FavouriteCubit(),
+        child: BlocBuilder<FavouriteCubit, FavouriteState>(
+          builder: (context, state) {
+            return Scaffold(
+              body: Padding(
+                padding: const EdgeInsets.all(AppPadding.p16),
+                child: context.read<FavouriteCubit>().savedListLength() == 0
+                    ? Center(
+                  child: Text(AppLocalizations.of(context).noSavedItems),
+                )
+                    : GridView.builder(
+                  itemCount:
+                  context.read<FavouriteCubit>().savedListLength(),
+                  itemBuilder: (context, index) {
+                    final videoModel = context
+                        .read<FavouriteCubit>()
+                        .getSavedVideos(index);
+                    if (videoModel.id == -1) {
+                      return const SizedBox();
+                    } else {
+                      return RelatedVideoContainer(
+                        videoModel: videoModel,
+                      );
+                    }
+                  },
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: crossCount == 0 ? 1 : crossCount,
+                    childAspectRatio: 1.2,
+                  ),
+                ),
+              ),
+            );
+          },
+        ));
   }
 }
