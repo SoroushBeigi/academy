bee618ba37040d0d0b93444eb0df2a0fbee65972
s.masoumbeigi <sbeygi712@gmail.com>
2024-06-23 10:17:41 +0330

    add content page mobile responsiveness and bug fixes

--- lib/src/features/new_content/presentation/pages/new_content_component.dart
+++ lib/src/features/new_content/presentation/pages/new_content_component.dart
@@ -23,69 +23,81 @@ class _NewContentComponentState extends State<NewContentComponent> {
   Widget build(BuildContext context) {
     final textLocalization = AppLocalizations.of(context);
     return BlocProvider(
-      create: (BuildContext context) => getIt<NewContentCubit>()..getAllContents(),
+      create: (BuildContext context) =>
+          getIt<NewContentCubit>()..getAllContents(),
       child: Padding(
         padding: const EdgeInsets.all(AppPadding.p16),
         child: BlocConsumer<NewContentCubit, NewContentState>(
           listener: (BuildContext context, NewContentState state) {
             state.whenOrNull(
               successAddNewContent: () => showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return Dialog(
-                      child: Padding(
-                        padding: const EdgeInsets.all(AppPadding.p16),
-                        child: SizedBox(
-                          width: MediaQuery.of(context).size.width * 0.4,
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              Text(textLocalization.successfulAddNewContent),
-                              Space.h16,
-                              ACElevatedButton(
-                                onTap: () {
-                                  context.pop();
-                                },
-                                child: Text(textLocalization.accept, style: Theme.of(context).textTheme.bodyMedium?.copyWith(color: Theme.of(context).colorScheme.surface),),
-                              )
-                            ],
-                          ),
+                context: context,
+                builder: (BuildContext context) {
+                  return Dialog(
+                    child: Padding(
+                      padding: const EdgeInsets.all(AppPadding.p16),
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.4,
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(textLocalization.successfulAddNewContent),
+                            Space.h16,
+                            ACElevatedButton(
+                              onTap: () {
+                                context.pop();
+                              },
+                              child: Text(
+                                textLocalization.accept,
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodyMedium
+                                    ?.copyWith(
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .surface),
+                              ),
+                            )
+                          ],
                         ),
                       ),
-                    );
-                  },
+                    ),
+                  );
+                },
               ),
             );
           },
           builder: (BuildContext context, state) {
             return state.whenOrNull(
-              loading: () => const ACLoading(),
-              success: () => SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Text(
-                      '${textLocalization.newContent}:',
-                      style: Theme.of(context).textTheme.titleMedium,
+                  loading: () => const ACLoading(),
+                  success: () => SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Text(
+                          '${textLocalization.newContent}:',
+                          style: Theme.of(context).textTheme.titleMedium,
+                        ),
+                        Space.h8,
+                        const NewContentFormWidget()
+                      ],
                     ),
-                    Space.h8,
-                    const NewContentFormWidget()
-                  ],
-                ),
-              ),
-            ) ?? Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Text(
-                  '${textLocalization.newContent}:',
-                  style: Theme.of(context).textTheme.titleMedium,
-                ),
-                Space.h8,
-                const NewContentFormWidget()
-              ],
-            );
+                  ),
+                ) ??
+                SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Text(
+                        '${textLocalization.newContent}:',
+                        style: Theme.of(context).textTheme.titleMedium,
+                      ),
+                      Space.h8,
+                      const NewContentFormWidget()
+                    ],
+                  ),
+                );
           },
-
         ),
       ),
     );
