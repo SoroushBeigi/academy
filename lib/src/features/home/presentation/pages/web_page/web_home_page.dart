6fe8c874870c485f61ea5de143ea58865a0943c0
s.masoumbeigi <sbeygi712@gmail.com>
2024-06-24 12:16:09 +0330

    web page shimmer effect

--- lib/src/features/home/presentation/pages/web_page/web_home_page.dart
+++ lib/src/features/home/presentation/pages/web_page/web_home_page.dart
@@ -1,20 +1,20 @@
+import 'dart:math';
+
 import 'package:academy/src/core/extensions/widget_extensions.dart';
 import 'package:academy/src/core/widgets/app_header.dart';
 import 'package:academy/src/di/di_setup.dart';
 import 'package:academy/src/features/core/core.dart';
 import 'package:academy/src/features/home/presentation/bloc/home_state.dart';
 import 'package:academy/src/features/search/presentation/cubit/search_cubit.dart';
-import 'package:academy/src/features/search/presentation/widgets/search_field.dart';
 import 'package:academy/src/features/video_details/presentation/pages/widgets/related_video/related_video_container.dart';
 import 'package:academy/content_entity.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:go_router/go_router.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:shimmer/shimmer.dart';
 
 import '../../../../../core/resources/resources.dart';
-import '../../../../main/presentation/bloc/main_cubit.dart';
 import '../../bloc/home_cubit.dart';
 
 class WebHomePage extends StatefulWidget {
@@ -48,7 +48,7 @@ class _WebHomePageState extends State<WebHomePage> {
       builder: (context, state) {
         return state.whenOrNull(
               initial: () => const ACLoading(),
-              loading: () => const ACLoading(),
+              loading: () => shimmerWidget(context),
               done: () => RefreshIndicator(
                 onRefresh: () => context.read<HomeCubit>().initial(),
                 child: SingleChildScrollView(
@@ -56,54 +56,13 @@ class _WebHomePageState extends State<WebHomePage> {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       ///page header
-                      const AppHeader(textFieldAutoFocus: false,textFieldEnabled: false,),
+                      const AppHeader(
+                        textFieldAutoFocus: false,
+                        textFieldEnabled: false,
+                      ),
 
                       ///categories
                       idleChips(context),
-
-                      // SingleChildScrollView(
-                      //   scrollDirection: Axis.horizontal,
-                      //   child: Row(
-                      //     children: List.generate(
-                      //       7,
-                      //       (index) => GestureDetector(
-                      //         onTap: () {
-                      //           VideoDetailsCubit.url = videoModels[index].url!;
-                      //           context.pushNamed('videoDetails',
-                      //               extra: videoModels[index]);
-                      //         },
-                      //         child: Container(
-                      //             padding: const EdgeInsets.all(12),
-                      //             margin:
-                      //                 const EdgeInsets.symmetric(horizontal: 8),
-                      //             decoration: BoxDecoration(
-                      //                 border: Border.all(
-                      //                     color: Colors.grey[800]!, width: 2),
-                      //                 borderRadius: BorderRadius.circular(30),
-                      //                 color: index % 2 == 0
-                      //                     ? Colors.red[500]
-                      //                     : Colors.red[800]),
-                      //             child: const Icon(
-                      //               Icons.live_tv,
-                      //               size: 35,
-                      //               color: Colors.white,
-                      //             )),
-                      //       ),
-                      //     ),
-                      //   ),
-                      // ),
-
-                      // child: GridView.builder(
-                      //   itemCount: HomeCubit.videos.length,
-                      //   itemBuilder: (context, index) => RelatedVideoContainer(
-                      //     margin: 8,
-                      //     videoModel: HomeCubit.videos[index],
-                      //   ),
-                      //   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      //     crossAxisCount: crossCount == 0 ? 1 : crossCount,
-                      //     childAspectRatio: 1,
-                      //   ),
-                      // ),
 
                       categorySection(
                           context, 'Live', HomeCubit.videos.sublist(0)),
@@ -162,25 +121,65 @@ class _WebHomePageState extends State<WebHomePage> {
       );
 
   idleChips(BuildContext context) => SingleChildScrollView(
-    scrollDirection: Axis.horizontal,
-    child: Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-        children: HomeCubit
-            .chips
-            .entries
-            .map(
-              (e) => Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                child: FilterChip(
-                  label: Text(e.key),
-                  onSelected: (_) {
-                    getIt<SearchCubit>().selectChip(e.key);
-                    context.go('/search', extra: {'isFromHome':true,'selectedChip': e.key});
-                  },
-                  selected: false,
+        scrollDirection: Axis.horizontal,
+        child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: HomeCubit.chips.entries
+                .map(
+                  (e) => Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: FilterChip(
+                      label: Text(e.key),
+                      onSelected: (_) {
+                        getIt<SearchCubit>().selectChip(e.key);
+                        context.go('/search',
+                            extra: {'isFromHome': true, 'selectedChip': e.key});
+                      },
+                      selected: false,
+                    ),
+                  ),
+                )
+                .toList()),
+      );
+
+  shimmerWidget(BuildContext context) {
+    final random = Random();
+    return Shimmer.fromColors(
+      baseColor: Theme.of(context).colorScheme.surfaceContainerHighest,
+      highlightColor: Theme.of(context).colorScheme.surfaceContainerLow,
+      period: const Duration(milliseconds: 1000),
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const AppHeader(
+              textFieldAutoFocus: false,
+              textFieldEnabled: false,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: List.generate(
+                10,
+                (index) => Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 4),
+                  child: FilterChip(
+                      label: SizedBox(
+                        width: random.nextDouble() * 100,
+                      ),
+                      onSelected: null),
                 ),
               ),
+            ),
+            Column(
+              children: List.generate(
+                3,
+                (_) => categorySection(
+                    context, '', List.generate(10, (_) => ContentEntity())),
+              ),
             )
-            .toList()),
-  );
+          ],
+        ),
+      ),
+    );
+  }
 }
