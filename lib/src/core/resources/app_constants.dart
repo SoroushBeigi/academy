b5dcf173386e38022f9ff8137da9ef739d1df6d0
s.masoumbeigi <sbeygi712@gmail.com>
2024-06-29 09:32:12 +0330

    search bar resize

--- lib/src/core/resources/app_constants.dart
+++ lib/src/core/resources/app_constants.dart
@@ -6,8 +6,8 @@ class AppConstants {
   static const String saveContentList = 'saveContentList';
   static const String languageKey = 'language';
   static const String themeKey = 'theme';
-  static const String baseUrl = 'http://172.16.251.80:8080';
-  static const String baseUrlWithoutPort = 'http://172.16.251.80';
+  static const String baseUrl = 'http://46.209.222.131:8080';
+  static const String baseUrlWithoutPort = 'http://46.209.222.131';
   static const String loggedInKey = 'loggedIn';
   static const String imageUrl =
       'https://fastly.picsum.photos/id/1044/200/200.jpg?hmac=HB3e6tTss6J_9wexZ1v1psMlccdyQIrHrrijUgWXFhg';
