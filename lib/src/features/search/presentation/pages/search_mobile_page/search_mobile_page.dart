50ed1f9b953797fc32bc23289b04253b57310afd
s.masoumbeigi <sbeygi712@gmail.com>
2024-06-29 11:26:31 +0330

    fixed search page mobile UI problems

--- lib/src/features/search/presentation/pages/search_mobile_page/search_mobile_page.dart
+++ lib/src/features/search/presentation/pages/search_mobile_page/search_mobile_page.dart
@@ -13,7 +13,8 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import '../../../../core/ui_kits/ui_kits.dart';
 
 class SearchMobilePage extends StatefulWidget {
-  const SearchMobilePage({required this.isFromHome,required this.selectedChip, super.key});
+  const SearchMobilePage(
+      {required this.isFromHome, required this.selectedChip, super.key});
 
   final bool? isFromHome;
   final String? selectedChip;
@@ -25,73 +26,73 @@ class SearchMobilePage extends StatefulWidget {
 class _SearchMobilePageState extends State<SearchMobilePage> {
   @override
   void initState() {
-    if(HomeCubit.chips.entries.isEmpty){
+    if (HomeCubit.chips.entries.isEmpty) {
       context.read<SearchCubit>().initialLoad();
     }
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context){
-        if(widget.isFromHome==true&& widget.selectedChip!=null){
+      create: (context) {
+        if (widget.isFromHome == true && widget.selectedChip != null) {
           return getIt<SearchCubit>()..selectChip(widget.selectedChip!);
-        }else{
+        } else {
           return getIt<SearchCubit>();
         }
       },
-
       child: BlocBuilder<SearchCubit, SearchState>(
         builder: (context, state) {
           return Scaffold(
             body: Padding(
               padding: const EdgeInsets.all(AppPadding.p16),
-              child: Stack(
-                children: [
-                  state.whenOrNull(
-                    loading: () => const ACLoading(),
-                        initial: () => SizedBox(
-                          height: MediaQuery.of(context).size.height,
-                          child: Center(
-                            child: Text(
-                                AppLocalizations.of(context).startSearching),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    SearchField(enabled: true, autoFocus: widget.isFromHome),
+                    AppSize.s8.heightSizeBox(),
+                    state.whenOrNull(
+                          foundVideos: (_) =>
+                              activeChips(context, HomeCubit.chips),
+                          initial: () => activeChips(context, HomeCubit.chips),
+                          chipsChanged: (chips) => activeChips(context, chips),
+                        ) ??
+                        const SizedBox(),
+                    state.whenOrNull(
+                          loading: () => const ACLoading(),
+                          initial: () => SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.5,
+                            child: Center(
+                              child: Text(
+                                  AppLocalizations.of(context).startSearching),
+                            ),
                           ),
-                        ),
-                        foundVideos: (videos) => Padding(
-                          padding: const EdgeInsets.only(top: 56),
-                          child: videos.isEmpty
-                              ? Center(
-                                  child: Text(AppLocalizations.of(context)
-                                      .noItemsFound),
+                          foundVideos: (videos) => videos.isEmpty
+                              ? SizedBox(
+                                  height:
+                                      MediaQuery.of(context).size.height * 0.5,
+                                  child: Center(
+                                    child: Text(AppLocalizations.of(context)
+                                        .noItemsFound),
+                                  ),
                                 )
                               : ListView.builder(
-                                  padding: const EdgeInsets.only(bottom: 100,top:100),
+                                  physics: const NeverScrollableScrollPhysics(),
+                                  padding: const EdgeInsets.only(bottom: 100),
                                   shrinkWrap: true,
                                   itemCount: videos.length,
-                                  itemBuilder: (context, index) =>
-                                      Container(
-                                        margin: const EdgeInsets.all(8),
-                                        child: RelatedVideoContainer(
-                                                                            videoModel: videos[index],
-                                                                          ),
-                                      ),
+                                  itemBuilder: (context, index) => Container(
+                                    margin: const EdgeInsets.all(8),
+                                    child: RelatedVideoContainer(
+                                      videoModel: videos[index],
+                                    ),
+                                  ),
                                 ),
-                        ),
-                      ) ??
-                      const SizedBox(),
-                  Column(
-                    children: [
-                      SearchField(enabled: true, autoFocus: widget.isFromHome),
-                      AppSize.s8.heightSizeBox(),
-                      state.whenOrNull(
-                        foundVideos: (_) => activeChips(context,HomeCubit.chips),
-                        initial: () => activeChips(context,HomeCubit.chips),
-                        chipsChanged: (chips) => activeChips(context,chips),
-                      ) ??
-                          const SizedBox(),
-                    ],
-                  ),
-                ],
+                        ) ??
+                        const SizedBox(),
+                  ],
+                ),
               ),
             ),
           );
@@ -101,42 +102,41 @@ class _SearchMobilePageState extends State<SearchMobilePage> {
   }
 
   idleChips(BuildContext context) => SingleChildScrollView(
-    scrollDirection: Axis.horizontal,
-    child: Row(
-        children: HomeCubit.chips.entries
-            .map(
-              (e) => Container(
-                margin: const EdgeInsets.only(bottom: 2),
-            padding: const EdgeInsets.symmetric(horizontal: 2),
-            child: FilterChip(
-              label: Text(e.key),
-              onSelected: (value) =>
-                  context.read<SearchCubit>().switchChips(e.key, value),
-              selected: false,
-            ),
-          ),
-        )
-            .toList()),
-  );
+        scrollDirection: Axis.horizontal,
+        child: Row(
+            children: HomeCubit.chips.entries
+                .map(
+                  (e) => Container(
+                    margin: const EdgeInsets.only(bottom: 2),
+                    padding: const EdgeInsets.symmetric(horizontal: 2),
+                    child: FilterChip(
+                      label: Text(e.key),
+                      onSelected: (value) =>
+                          context.read<SearchCubit>().switchChips(e.key, value),
+                      selected: false,
+                    ),
+                  ),
+                )
+                .toList()),
+      );
 
-  activeChips(BuildContext context,Map<String,bool> chips) => SingleChildScrollView(
-    scrollDirection: Axis.horizontal,
-    child: Row(
-        children: chips.entries
-            .map(
-              (e) => Container(
-                margin: const EdgeInsets.only(bottom: 2),
-            padding: const EdgeInsets.symmetric(
-                horizontal: 2),
-            child: FilterChip(
-              label: Text(e.key),
-              onSelected: (value) => context
-                  .read<SearchCubit>()
-                  .switchChips(e.key, value),
-              selected: chips[e.key] ?? false,
-            ),
-          ),
-        )
-            .toList()),
-  );
+  activeChips(BuildContext context, Map<String, bool> chips) =>
+      SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
+            children: chips.entries
+                .map(
+                  (e) => Container(
+                    margin: const EdgeInsets.only(bottom: 2),
+                    padding: const EdgeInsets.symmetric(horizontal: 2),
+                    child: FilterChip(
+                      label: Text(e.key),
+                      onSelected: (value) =>
+                          context.read<SearchCubit>().switchChips(e.key, value),
+                      selected: chips[e.key] ?? false,
+                    ),
+                  ),
+                )
+                .toList()),
+      );
 }
