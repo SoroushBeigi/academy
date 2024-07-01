df9639b5ff9f73ce7e396417688347ef499bdb6b
s.masoumbeigi <sbeygi712@gmail.com>
2024-07-01 12:25:49 +0330

    authentication page additional fields localization

--- lib/src/features/auth/presentation/screens/auth_screen.dart
+++ lib/src/features/auth/presentation/screens/auth_screen.dart
@@ -1,3 +1,4 @@
+import 'package:academy/src/core/locale_provdier.dart';
 import 'package:academy/src/di/di_setup.dart';
 import 'package:academy/src/features/auth/presentation/cubit/auth_cubit.dart';
 import 'package:academy/src/features/new_content/new_content.dart';
@@ -11,6 +12,7 @@ class AuthScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final localizations = AppLocalizations.of(context);
     final cubit = getIt<AuthCubit>();
+    final localeProvider = context.read<LocaleProvider>();
     return BlocProvider(
       create: (context) {
         return AuthCubit();
@@ -25,12 +27,15 @@ class AuthScreen extends StatelessWidget {
           confirmPasswordHint: localizations.confirmPassword,
           flushbarTitleError: localizations.error,
           confirmPasswordError: localizations.confirmPasswordError,
+          additionalSignUpFormDescription: localizations.additionalSignUpFormDescription,
+          additionalSignUpSubmitButton: localizations.submit,
+          goBackButton: localizations.back,
         ),
         logo: 'assets/icon.png',
-        additionalSignupFields: const [
+        additionalSignupFields: [
           UserFormField(
-            keyName: 'username',
-            icon: Icon(Icons.person),
+            keyName: AppConstants.isFa? 'نام‌کاربری' : 'username',
+            icon: const Icon(Icons.person),
           )
         ],
         onLogin: (p0)async {
@@ -55,6 +60,39 @@ class AuthScreen extends StatelessWidget {
             return result;
           }
         },
+        headerWidget: Container(
+          margin: const EdgeInsets.all(4),
+          height: 60,
+          child: InkWell(
+            borderRadius: BorderRadius.circular(12),
+            onTap: () {
+              if (localeProvider.locale == const Locale('en')) {
+                localeProvider.changeLocale(const Locale('fa'));
+              } else {
+                localeProvider.changeLocale(const Locale('en'));
+              }
+            },
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    AppLocalizations.of(context).appLanguage,
+                  ),
+                  Padding(
+                    padding:
+                    const EdgeInsets.symmetric(horizontal: 16),
+                    child: Text(
+                      AppLocalizations.of(context).oppositeLanguage,
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
